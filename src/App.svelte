ec6a64c433b095e34aaee61397131c0cebc79309
evariste1963 <65227896+evariste1963@users.noreply.github.com>
2022-08-25 07:58:02 +0100

    1st push from work

--- src/App.svelte
+++ src/App.svelte
@@ -1,20 +1,16 @@
 <script>
-	import Header from './components/Header.svelte'
-	import Footer from './components/Footer.svelte'
+  import Header from "./components/Header.svelte";
+  import Footer from "./components/Footer.svelte";
 </script>
-
 
 <Header />
 
-<main>
-
-	<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.  </p>
-	
-</main>
+<main />
 <Footer />
+
 <style>
-	main{
-		max-width: 960px;
-		margin: 40px auto;
-	}
+  main {
+    max-width: 960px;
+    margin: 40px auto;
+  }
 </style>
