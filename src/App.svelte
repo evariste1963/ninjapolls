a8c95ae62df6925e70de63e756431b7aee3f1e6f
evariste1963 <evariste@hotmail.co.uk>
2022-08-28 16:18:19 +0100

    course link in App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -1,3 +1,4 @@
+<!-- course like => https://www.youtube.com/watch?v=zojEMeQGGHs&list=PL4cUxeGkcC9hlbrVO_2QFVqVPhlZmz7tO&index=1 -->
 <script>
   import Header from "./components/Header.svelte";
   import Footer from "./components/Footer.svelte";
