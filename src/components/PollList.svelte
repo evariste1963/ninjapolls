80d15c319dc8606e1fe58c0defe89af22af15e22
evariste1963 <65227896+evariste1963@users.noreply.github.com>
2022-08-30 15:55:35 +0100

    no Polls to show message when there are no....

--- src/components/PollList.svelte
+++ src/components/PollList.svelte
@@ -19,16 +19,29 @@
 </script>
 
 <!-- $PollStore below subcribes and unsubscribes from the store implicitly -->
-<div class="poll-list">
-  {#each $PollStore as poll (poll.id)}
-    <PollDetails {poll} />
-  {/each}
-</div>
+
+{#if $PollStore.length < 1}
+  <h4 style="text-align:center"
+    >there are no Polls to show, try creating one!</h4
+  >
+{:else}
+  <div class="poll-list">
+    {#each $PollStore as poll (poll.id)}
+      <PollDetails {poll} />
+    {/each}
+  </div>
+{/if}
 
 <style>
   .poll-list {
     display: grid;
     grid-template-columns: 1fr 1fr;
     grid-gap: 20px;
+  }
+
+  h4 {
+    font-style: italic;
+    color: #272727;
+    font-weight: 400;
   }
 </style>
