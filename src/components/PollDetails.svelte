f4ac9813b0d8253091579b2f5d974327a9f54e34
evariste1963 <evariste@hotmail.co.uk>
2022-08-31 20:04:41 +0100

    final commit --> course end

--- src/components/PollDetails.svelte
+++ src/components/PollDetails.svelte
@@ -2,18 +2,26 @@
   import PollStore from "../stores/PollStore";
   import Card from "../shared/Card.svelte";
   import Button from "../shared/Button.svelte";
+  import { tweened } from "svelte/motion";
+
   export let poll;
 
   //reactive values
   $: totalVotes = poll.votesA + poll.votesB;
-  $: percentA = Math.floor((100 / totalVotes) * poll.votesA);
-  $: percentB = Math.floor((100 / totalVotes) * poll.votesB);
+  $: percentA = Math.floor((100 / totalVotes) * poll.votesA) || 0;
+  $: percentB = Math.floor((100 / totalVotes) * poll.votesB) || 0;
 
+  //teaned percentages
+  const tweenedA = tweened(0);
+  const tweenedB = tweened(0);
+
+  $: tweenedA.set(percentA);
+  $: tweenedB.set(percentB);
   //handling Votes
   const handleVote = (option, id) => {
-    PollStore.update(currentPolls => {
+    PollStore.update((currentPolls) => {
       let copiedPolls = [...currentPolls];
-      let upVotedPoll = copiedPolls.find(poll => poll.id == id);
+      let upVotedPoll = copiedPolls.find((poll) => poll.id == id);
 
       option === "a" ? upVotedPoll.votesA++ : upVotedPoll.votesB++;
 
@@ -21,9 +29,9 @@
     });
   };
 
-  const handleDelete = id => {
-    PollStore.update(currentPolls => {
-      return currentPolls.filter(poll => poll.id !== id);
+  const handleDelete = (id) => {
+    PollStore.update((currentPolls) => {
+      return currentPolls.filter((poll) => poll.id !== id);
     });
   };
 </script>
@@ -40,11 +48,11 @@
     </div>
     <p>Total votes: {totalVotes}</p>
     <div class="answer" on:click={() => handleVote("a", poll.id)}>
-      <div class="percent percent-a" style="width:{percentA}%" />
+      <div class="percent percent-a" style="width:{$tweenedA}%" />
       <span>{poll.answerA} ({poll.votesA})</span>
     </div>
     <div class="answer" on:click={() => handleVote("b", poll.id)}>
-      <div class="percent percent-b" style="width:{percentB}%" />
+      <div class="percent percent-b" style="width:{$tweenedB}%" />
       <span>{poll.answerB} ({poll.votesB})</span>
     </div>
   </div>
