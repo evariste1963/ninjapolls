af086b3ad1573a961b929603e40c899b739a652d
evariste1963 <evariste@hotmail.co.uk>
2023-01-05 20:07:07 +0000

    new pc

--- src/stores/PollStore.js
+++ src/stores/PollStore.js
@@ -9,6 +9,14 @@ const PollStore = writable([
     votesA: 9,
     votesB: 15,
   },
+  {
+    id: 2,
+    question: "big or small?",
+    answerA: "bigg...ish!",
+    answerB: "yeah, small!",
+    votesA: 45,
+    votesB: 90,
+  },
 ]);
 
 export default PollStore;
